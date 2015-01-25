1dae54987b7db76dcad1518072243ee6cfc3873e
Dan Tasse <dan.tasse@gmail.com>
2015-01-25 11:59:18 -0500

    Tweak: little bigger font.

--- src/main.c
+++ src/main.c
@@ -51,8 +51,7 @@ static void update_time() {
 }
 
 static void main_window_load(Window *window) {
-  s_current_pokemon = gbitmap_create_with_png_resource(RESOURCE_ID_POKEMON_0);
-  // TODO make it start from the real time, not from 0.
+  s_current_pokemon = gbitmap_create_with_png_resource(RESOURCE_ID_POKEMON_0); // will be overwritten almost instantly
   s_bitmap_layer = bitmap_layer_create(GRect(0, 0, 144, 144));
   bitmap_layer_set_bitmap(s_bitmap_layer, s_current_pokemon);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_layer));
@@ -61,7 +60,7 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorClear);
   text_layer_set_text(s_time_layer, "");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
@@ -85,7 +84,7 @@ static void init() {
   
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  
+
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   update_time();
